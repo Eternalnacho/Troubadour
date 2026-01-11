1723b80248e65a275999da5f0e19e79237997104
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-01-10 21:04:11 -0600

    killing off lovely patches

--- src/hooks/collection_hooks.lua
+++ src/hooks/collection_hooks.lua
@@ -1,5 +1,3 @@
--- HOOKING COLLECTION FUNCTIONS
-
 TRO.UI.get_page_num = true
 local card_collection_uibox = SMODS.card_collection_UIBox
 SMODS.card_collection_UIBox = function(_pool, rows, args)
@@ -14,6 +12,17 @@ function G.FUNCS.your_collection(...)
     TRO.UI.targets.added_target = ''
     TRO.config_from_coll = nil
     return old_FUNCS_your_collection(...)
+end
+
+local uibox_your_collection = create_UIBox_your_collection
+create_UIBox_your_collection = function()
+  local ret = uibox_your_collection()
+  -- Changing the back button function if collection was called from the button
+  local back_button = ret.nodes[1].nodes[1].nodes[2]
+  back_button.config.button = TRO.coll_from_button and 'exit_search_collection' or back_button.config.button
+  -- Adding the Auto-reroll UI
+  table.insert(ret.nodes[1].nodes[1].nodes[1].nodes, (TRO.coll_from_button or next(TRO.collection_targets)) and TRO.UIDEF.auto_reroll_UI())
+  return ret
 end
 
 local old_FUNCS_exit_overlay_menu = G.FUNCS.exit_overlay_menu
