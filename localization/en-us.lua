46bb4fbe91f9e5b0fd8c5265d18498390526a504
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-01-15 22:16:55 -0600

    tooltip fix for swapping controls

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -26,8 +26,7 @@ return {
           'Use {C:attention}Left-Click{} to {C:green}Enable{}/{C:attention}Disable{} Mod',
           'Use {C:attention}Right-Click{} to {C:attention}View{} Mod',
           '{C:inactive,br:2}-------------',
-          '{C:attention,s:0.7}Shift{s:0.7} + {C:attention,s:0.7}Left-Click{s:0.7} is the',
-          '{s:0.7}same as {C:attention,s:0.7}Right-Click{s:0.7}'
+          '{C:attention,s:0.7}Shift{s:0.7} + {C:attention,s:0.7}Left-Click{s:0.7} is the {s:0.7}same as {C:attention,s:0.7}Right-Click{s:0.7}'
         }
       },
       TRO_ctrls_extra_i = {
