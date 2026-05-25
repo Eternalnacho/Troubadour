0d0d77ec720b5884de0cfae72ddd2ea5f42ada8f
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-05-25 02:17:54 -0500

    specifying exactly what it is the loader found

--- atlases.lua
+++ atlases.lua
@@ -21,7 +21,7 @@ for _, mod in pairs(SMODS.Mods) do
       local full_path = mod.path .. 'assets/' .. G.SETTINGS.GRAPHICS.texture_scaling .. 'x/' .. file_path .. '.png'
       local file_data, error = NFS.newFileData(full_path)
       if not error then
-        print("File location found: "..full_path)
+        print("Mod Icon location found: "..full_path)
         local image_data = assert(love.image.newImageData(file_data),
                 ('Failed to initialize image data for Atlas %s'):format(file_path))
         local px, py = image_data:getDimensions()
