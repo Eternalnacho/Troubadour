f5e8176c409022d0a417203a234a14f879dd0d62
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-07-04 00:26:42 -0500

    re-adding the warning icon to mods with load issues

--- src/ui/mods_tab.lua
+++ src/ui/mods_tab.lua
@@ -172,6 +172,14 @@ function Troubadour.UIDEF.getModTagInfo(mod)
   local tag_atlas = mod.prefix and mod.prefix .. '_modicon' or 'modicon'
 
   if not mod.can_load then
+    if next(mod.load_issues.dependencies)
+        or next(mod.load_issues.conflicts)
+        or mod.load_issues.outdated
+        or mod.load_issues.version_mismatch
+        or mod.load_issues.main_file_not_found
+        or next(mod.load_issues.prefix_conflicts or {}) then
+      tag_atlas = "mod_tags"
+    end
     if mod.disabled and not mod.icon_path then
       tag_atlas = 'TRO_' .. mod.id .. '_modicon'
     end
